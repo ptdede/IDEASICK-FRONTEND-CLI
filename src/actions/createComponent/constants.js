3b743982acb971939c68a79518897c94bf31cdeb
Dede Adhyatmika <putu.dede@gmail.com>
2018-08-28 06:33:47 +0800

    clean up

--- src/actions/createComponent/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var COMPONENT_NAME = "COMPONENT_NAME";
-exports.COMPONENT_NAME = COMPONENT_NAME;
-var COMPONENT_TEST = "COMPONENT_TEST";
-exports.COMPONENT_TEST = COMPONENT_TEST;
-var DEFAULT_PATH = process.cwd() + "/src/components";
-exports.DEFAULT_PATH = DEFAULT_PATH;
--- src/header.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var chalk_1 = require("chalk");
-var figlet = require("figlet");
-var initHeader = function () {
-    console.log(chalk_1.default.green(figlet.textSync("IDEASICK")));
-};
-exports.default = initHeader;
