4920cfa3333ec12bbdac67e41ec6d9f401945ecb
Dede Adhyatmika <putu.dede@gmail.com>
2018-09-03 22:20:16 +0800

    add styled component template

--- src/actions/welcome/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var COMPONENT = "COMPONENT";
-exports.COMPONENT = COMPONENT;
-var CONTAINER = "CONTAINER";
-exports.CONTAINER = CONTAINER;
-var SAY_HI = "SAY_HI";
-exports.SAY_HI = SAY_HI;
